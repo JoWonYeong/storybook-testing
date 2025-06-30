732730d6f1f5153acad0e26ca88a054b174c7f28
JoWonYeong <wswy17@gmail.com>
2025-06-30 16:05:09 +0900

    ➕ [Chore] : 테스트 러너 사용을 위한 playwright설치

--- src/components/InboxScreen.stories.jsx
+++ src/components/InboxScreen.stories.jsx
@@ -1,3 +1,46 @@
+// import InboxScreen from './InboxScreen';
+
+// import store from '../lib/store';
+
+// import { http, HttpResponse } from 'msw';
+
+// import { MockedState } from './TaskList.stories';
+
+// import { Provider } from 'react-redux';
+
+// export default {
+//   component: InboxScreen,
+//   title: 'InboxScreen',
+//   decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+//   tags: ['autodocs'],
+// };
+
+// export const Default = {
+//   parameters: {
+//     msw: {
+//       handlers: [
+//         http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
+//           return HttpResponse.json(MockedState.tasks);
+//         }),
+//       ],
+//     },
+//   },
+// };
+
+// export const Error = {
+//   parameters: {
+//     msw: {
+//       handlers: [
+//         http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
+//           return new HttpResponse(null, {
+//             status: 403,
+//           });
+//         }),
+//       ],
+//     },
+//   },
+// };
+
 import InboxScreen from './InboxScreen';
 
 import store from '../lib/store';
@@ -7,6 +50,13 @@ import { http, HttpResponse } from 'msw';
 import { MockedState } from './TaskList.stories';
 
 import { Provider } from 'react-redux';
+
+import {
+  fireEvent,
+  waitFor,
+  within,
+  waitForElementToBeRemoved,
+} from '@storybook/test';
 
 export default {
   component: InboxScreen,
@@ -24,6 +74,18 @@ export const Default = {
         }),
       ],
     },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    // Waits for the component to transition from the loading state
+    await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+    // Waits for the component to be updated based on the store
+    await waitFor(async () => {
+      // Simulates pinning the first task
+      await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+      // Simulates pinning the third task
+      await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+    });
   },
 };
 
